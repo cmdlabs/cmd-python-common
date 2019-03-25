c63de92eb0e87d9ee96e05891d0787009fdaac27
Chris Langton <chris.langton@cmdsolutions.com.au>
2019-03-25 06:30:28 +0000

    retry deco

--- runcmd/__init__.py
+++ runcmd/__init__.py
@@ -8,6 +8,7 @@ try:
 except ImportError:
     from io import StringIO
 import time
+from functools import wraps
 from argparse import ArgumentParser
 from runcmd.logging_helper import get_logger
 
@@ -59,3 +60,41 @@ def generate_processed_timestamp():
     Generates a timestamp to mark components as processed.
     """
     return time.strftime("%Y%m%d_%H%M%S", time.localtime())
+
+
+def retry(exception_to_check, tries=4, delay=3, backoff=2, logger=None):
+    """
+    :param exception_to_check: the exception to check. may be a tuple of exceptions to check
+    :type exception_to_check: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+
+    def deco_retry(func): #pylint: disable=missing-docstring
+        @wraps(func)
+        def func_retry(*args, **kwargs): #pylint: disable=missing-docstring
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return func(*args, **kwargs)
+                except exception_to_check as err_obj:
+                    msg = "%s, Retrying in %d seconds..." % (str(err_obj), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+                except Exception as err_obj: #pylint: disable=broad-except
+                    logger.exception(err_obj)
+                    break
+            return func(*args, **kwargs)
+
+        return func_retry # true decorator
+
+    return deco_retry
